e71f776d0076756e96e1068807f2ed022466a4ff
braga9 <braga.gabriel1000@gmail.com>
2018-10-25 18:52:09 -0300

    Converts pdf to .png refs fga-eps-mds/2018.2-Lino#134
    Co-authored-by: braga9 <braga.gabriel1000@gmail.com>
    Co-authored-by: pedro-prp <peddroprp@gmail.com>

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -6,22 +6,10 @@ import datetime
 from tabula import convert_into
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from pdf2image import convert_from_path
 
-#gets current date and split year and month to get current period
-ts = time.time()
-st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m')
-st = st.split('-')
-year = st[0]
-month = int(st[1])
-if month >= 1 and month <=6:
-    semester = 1
-else:
-    semester = 2
-
-period = str(semester) + '\u00ba/' + str(year)
 DOWNLOAD_PATH = './downloads/'
 OUTPUT_PATH = './outputs/'
-
 
 class TheCrawler():
     def __init__(self):
@@ -30,6 +18,7 @@ class TheCrawler():
     def runCrawler(self):
         self.process.crawl('registration')
         self.process.start()
+
 class JsonReader():
     def __init__(self):
         with open('result.json') as f:
@@ -38,11 +27,29 @@ class JsonReader():
 class PdfReader():
     def __init__(self):
         self.data = JsonReader()
-    def downloadRegistration(self,period):
+
+    def getCurrentPeriod(self):
+        #gets current date and split year and month to get current period
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m')
+        st = st.split('-')
+        year = st[0]
+        month = int(st[1])
+        if month >= 1 and month <=6:
+            semester = 1
+        else:
+            semester = 2
+        
+        period = str(semester) + '\u00ba/' + str(year)
+        return period
+
+    def downloadRegistration(self):
         #Downloads pdf from 'result.json'
+        period = self.getCurrentPeriod()
         data = self.data
         if not os.path.exists(OUTPUT_PATH):
             os.mkdir(OUTPUT_PATH)
+
         for item in data.body:
             if "%09" in item['url']:
                 url = item['url'].replace("%09","")
@@ -52,14 +59,16 @@ class PdfReader():
             if period in item['text']:
                 pdf = pdfx.PDFx(url)
                 pdf.download_pdfs(DOWNLOAD_PATH)
-                fileName = 'cal_2018_2'
-                convert_into(
-                    f'{DOWNLOAD_PATH}{fileName}.pdf',
-                    f'{OUTPUT_PATH}{fileName}.tsv',
-                    output_format='tsv')
+                fileName = url.split('/')
+                fileName = fileName.pop()
+                fileName = fileName.replace('.pdf','')
+                pdf = convert_from_path(f'{DOWNLOAD_PATH}{fileName}.pdf', 300)
+                for page in pdf:
+                    page.save(f'{OUTPUT_PATH}{fileName}.png', 'PNG')
 
-crawl = TheCrawler()
-crawl.runCrawler()
-pdf = PdfReader()
-pdf.downloadRegistration(period)
+if __name__ == '__main__':
+    crawl = TheCrawler()
+    crawl.runCrawler()
+    pdf = PdfReader()
+    pdf.downloadRegistration()
 
