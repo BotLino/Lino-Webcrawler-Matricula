6143f54ae0fd714caa60839d770842887f1627c3
pedro-prp <peddroprp@gmail.com>
2018-11-10 10:20:16 -0200

    Add test to a valid json link

--- crawler/tests/test_server.py
+++ crawler/tests/test_server.py
@@ -1,6 +1,9 @@
 from freezegun import freeze_time
+from pprint import pprint
+import requests
+import time
+import datetime
 import json
-import pytest
 
 class TestServer():
 
@@ -8,3 +11,30 @@ class TestServer():
     def test_get_image(self,test_client,json_result_content, tmpdir):
         res = test_client.get('/registration/downloadPdf')
         assert res.status_code == 200
+
+    @freeze_time('2018-11-09')
+    def test_json_link(self,test_client,json_result_content,tmpdir):
+        res = test_client.get('/registration/downloadPdf')
+
+        with open('result.json','r') as result_json:
+            data = json.loads(result_json.read())
+            ts = time.time()
+            st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m')
+            st = st.split('-')
+            year = st[0]
+            month = int(st[1])
+            if month >= 1 and month <= 6:
+                semester = 1
+            else:
+                semester = 2
+
+            period = str(semester) + '\u00ba/' + str(year)
+            for item in data:
+                if period in item['text']:
+                    url = item['url']
+                    if "%09" in url:
+                        url = url.replace("%09", "")
+                    res = requests.get(url)
+                    assert res.status_code == 200
+
+        pprint(data)
